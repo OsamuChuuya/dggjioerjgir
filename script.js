4a7ccfe5328f03941adac777ec21beaea9508eda
OsamuChuuya <osamuchuuya26@gmail.com>
2024-12-17 21:38:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let eren = prompt('String typedagi malumot kiriting');
+let mike = Number(prompt('Number typedagi malumot kiriting'));
+let mik = Boolean(prompt('Boolean typedagi malumot kiriting'))
+
+alert('Hamma malumotlarni togri kiritganizga ishonchingiz komilmi?');
+
+let eren1 = typeof eren;
+let mike1 = typeof mike;
+let mik1 =  typeof mik;
+
+alert(`siz kiritgan ${eren} sozi ${eren1} typega tegishli \n siz kiritgan ${mike} sozi ${mike1} typega tegishli \n siz kiritgan ${mik} sozi ${mik1} typega tegishli`);
